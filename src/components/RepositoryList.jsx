eb70ddc32ba9b852a5c4ada765724029049c7250
Thiago Kim Nakamura <thiagokim17@gmail.com>
2021-12-04 18:17:03 -0300

    fix(api): consumindo api do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react"
 import { RepositoryItem } from "./RepositoryItem"
 
 import '../styles/repositories.scss'
+
+//https://api.github.com/users/thiagokim17/repos
 
 const repository = {
     name: 'unform2',
@@ -9,15 +12,30 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/thiagokim17/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data.map(repo => ({
+                id: repo.id,
+                name: repo.name,
+                description: repo.description,
+                link: repo.svn_url
+            }))))
+    }, [])
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
 
             <ul>
-                <RepositoryItem reporitory={repository} />
-                <RepositoryItem reporitory={repository} />
-                <RepositoryItem reporitory={repository} />
-                <RepositoryItem reporitory={repository} />
+                {
+                    repositories.map(repo => {
+                        return    <RepositoryItem key={repo.id} reporitory={repo} />
+                    })
+                }
+
             </ul>
         </section>
     )
